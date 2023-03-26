cd6f7a1d1ec9f471f2a422a2aad7dbb408444ed6
Adel N. Daniel <adel.daniel.222@gmail.com>
2023-03-26 22:41:28 +0200

    Generate Starter Project

---
+++ lib/news_change_notifier.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:foul_and_tamia/article.dart';
+import 'package:foul_and_tamia/news_services.dart';
+
+class NewsChangeNotifier extends ChangeNotifier {
+  final NewsService _newsService;
+
+  NewsChangeNotifier(this._newsService);
+
+  List<Article> _articles = [];
+
+  List<Article> get articles => _articles;
+
+  bool _isLoading = false;
+  bool get isLoading => _isLoading;
+
+  Future<void> getArticles() async {
+    // TODO: Implement
+  }
+}
