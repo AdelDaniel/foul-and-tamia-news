1688cd8791fb90e91b9288f995511199dba1491f
Adel N. Daniel <adel.daniel.222@gmail.com>
2023-06-07 12:06:49 +0300

    Add firebase Init in main method

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:foul_and_tamia/firebase_options.dart';
 import 'package:foul_and_tamia/news_change_notifier.dart';
 import 'package:foul_and_tamia/news_services.dart';
 import 'package:foul_and_tamia/pages/news_page.dart';
 import 'package:provider/provider.dart';
 
-void main() => runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
